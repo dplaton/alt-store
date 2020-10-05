42bcca1fb86213fcfcd0321f694bb408695c2975
Daniel Platon <platon@adobe.com>
2020-10-05 17:46:35 +0300

    First commit:
    * add the required dependencies and styling
    * make it work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+    CommerceApp,
+    Portal,
+    ConfigContextProvider,
+    Cart,
+    CartTrigger,
+    AccountContainer,
+} from '@adobe/aem-core-cif-react-components';
+import '../node_modules/@adobe/aem-core-cif-react-components/dist/main.css';
 import './App.css';
+import {I18nextProvider} from 'react-i18next';
+import i18n from './i18n';
+import partialConfig from './config';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {storeView, graphqlEndpoint} = document.querySelector('body').dataset;
+    const {mountingPoints, pagePaths} = partialConfig;
+    const config = {
+        ...partialConfig,
+        storeView,
+        graphqlEndpoint,
+    };
+
+    return (
+        <I18nextProvider i18n={i18n} defaultNS="common">
+            <ConfigContextProvider config={config}>
+                <div className="App">
+                    <header className="App-header">
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                    </header>
+                    <CommerceApp>
+                        <Portal selector={mountingPoints.minicart}>
+                            <Cart />
+                        </Portal>
+                        <Portal selector={mountingPoints.cartTrigger}>
+                            <CartTrigger />
+                        </Portal>
+                        <Portal selector={mountingPoints.accountContainer}>
+                            <AccountContainer />
+                        </Portal>
+                    </CommerceApp>
+                </div>
+            </ConfigContextProvider>
+        </I18nextProvider>
+    );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+    mountingPoints: {
+        accountContainer: '.header .miniaccount',
+        addressBookContainer: '#addressbook',
+        authBarContainer: 'aside.navigation__root #miniaccount',
+        cartTrigger: '.header .cart-trigger',
+        navPanel: 'aside.navigation__root',
+        bundleProductOptionsContainer: '#bundle-product-options',
+        minicart: '#minicart',
+        accountDetails: '#accountdetails',
+        resetPasswordPage: '#reset-password-page',
+    },
+    pagePaths: {
+        addressBook: '/content/venia/us/en/my-account/address-book.html',
+        accountDetails: '/content/venia/us/en/my-account/account-details.html',
+        resetPassword: '/content/venia/us/en/reset-password.html',
+    },
+};
